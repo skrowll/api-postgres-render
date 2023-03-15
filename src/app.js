03622abf86e4d48ed8172ffbd6ed5dede446fd37
skrowll <lucasdejesus.rocha@hotmail.com>
2023-03-15 20:53:41 -0300

    feat: install and use cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require("express-async-errors");
 const express = require('express');
+const cors = require('cors');
 const { sequelize } = require('./database/models');
 
 const routers = require('./routers');
@@ -9,6 +10,7 @@ const errorHandler = require('./middlewares/error.middleware')
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(routers);
 
